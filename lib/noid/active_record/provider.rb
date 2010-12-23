54b9d4a864ff2f5a74aa08059943d49cf0e9843e
Chris Beer <chris@cbeer.info>
2010-12-23 13:52:26 -0500

    or not..

--- lib/noid/active_record/provider.rb
+++ lib/noid/active_record/provider.rb
@@ -1,17 +1,26 @@
 module Noid::ActiveRecord
    module Provider
      def self.included base
-       base.send :attr_accessor, :identifier_field
-       base.send :attr_accessor, :minter
+       base.extend(ClassMethods)
        base.send :before_create, :mint_identifier
      end
 
+     module ClassMethods
+       def identifier_field value
+         @@identifier_field = value
+       end
+
+       def minter value
+         @@minter = value
+       end
+     end
+
      def minter
-       self.class.minter
+       @@minter
      end
 
      def mint_identifier
-       self.send "#{@identifier_field}=", self.minter.mint
+       self.send "#{@@identifier_field}=", self.minter.mint
      end
 
   end

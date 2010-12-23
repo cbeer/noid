32668515f1d10a16f91eb8b6e98f7f009a1331d9
Chris Beer <chris@cbeer.info>
2010-12-23 14:38:42 -0500

    re-write active record mixin

--- lib/noid/active_record/provider.rb
+++ lib/noid/active_record/provider.rb
@@ -1,26 +1,35 @@
 module Noid::ActiveRecord
    module Provider
      def self.included base
-       base.extend(ClassMethods)
        base.send :before_create, :mint_identifier
+    #   base.send :attr_accessor, :minter
+       class << base
+         def minter value = nil
+           @minter = value if value
+           @minter
+         end
+
+         def identifier_field value = nil
+           @identifier_field = value if value
+           @identifier_field
+         end
+
+         def persistence_path
+           File.join RAILS_ROOT, "db/#{self.to_s}_NOID.js"
+         end
+       end
      end
 
-     module ClassMethods
-       def identifier_field value
-         @@identifier_field = value
-       end
-
-       def minter value
-         @@minter = value
-       end
+     def minter= value
+       @minter = value
      end
 
      def minter
-       @@minter
+       @minter || self.class.minter
      end
 
      def mint_identifier
-       self.send "#{@@identifier_field}=", self.minter.mint
+       self.send "#{self.class.instance_variable_get('@identifier_field')}=", self.minter.mint unless self.attribute_present? self.class.instance_variable_get('@identifier_field')
      end
 
   end

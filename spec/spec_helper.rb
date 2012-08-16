b4b9adf4af11bd10870e1ad5c5df101fb30d9fd8
Chris Beer <cabeer@stanford.edu>
2012-08-15 21:42:01 -0700

    Fixup rake + rspec for 1.8 + 1.9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require 'rubygems'
 require 'bundler/setup'
+
+if ENV['COVERAGE'] and RUBY_VERSION =~ /^1.9/
+  require 'simplecov'
+
+  SimpleCov.start
+end
+
 require 'noid'
 
 RSpec.configure do |config|

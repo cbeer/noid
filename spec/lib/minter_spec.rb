4fd130b3cb06be68a47c93dce35e0fbd6ade0055
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2015-07-31 10:25:40 -0700

    Convert specs to RSpec 2.99.2 syntax with Transpec
    This conversion is done by Transpec 3.1.1 with the following command:
        transpec
    
    * 40 conversions
        from: obj.should
          to: expect(obj).to
    
    * 25 conversions
        from: == expected
          to: eq(expected)
    
    * 15 conversions
        from: =~ /pattern/
          to: match(/pattern/)
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/lib/minter_spec.rb
+++ spec/lib/minter_spec.rb
@@ -3,114 +3,114 @@ require 'spec_helper'
 describe Noid::Minter do
   it "should mint a few random 3-digit numbers" do
     minter = Noid::Minter.new(:template => '.rddd')
-    minter.mint.should =~ /\d\d\d/
+    expect(minter.mint).to match(/\d\d\d/)
   end
 
   it "should mint random 3-digit numbers, stopping after the 1000th" do
     minter = Noid::Minter.new(:template => '.rddd')
-    1000.times { minter.mint.should =~ /^\d\d\d$/ }
+    1000.times { expect(minter.mint).to match(/^\d\d\d$/) }
     expect { minter.mint }.to raise_exception
   end
 
   it "should mint sequential numbers without limit, adding new digits as needed" do
     minter = Noid::Minter.new(:template => '.zd')
-    minter.mint.should == "0"
-    999.times { minter.mint.should =~ /\d/ }
-    minter.mint.should == "1000"
+    expect(minter.mint).to eq("0")
+    999.times { expect(minter.mint).to match(/\d/) }
+    expect(minter.mint).to eq("1000")
   end
 
   it "should mint random 4-digit numbers with constant prefix bc" do
     minter = Noid::Minter.new(:template => 'bc.rdddd')
-    1000.times { minter.mint.should =~ /^bc\d\d\d\d$/ }
+    1000.times { expect(minter.mint).to match(/^bc\d\d\d\d$/) }
   end
 
   it "should mint sequential 2-digit numbers with constant prefix 8rf" do
     minter = Noid::Minter.new(:template => '8rf.sdd')
-    minter.mint.should == "8rf00"
-    10.times { minter.mint.should =~ /^8rf\d\d$/ }
-    minter.mint.should == "8rf11"
-    88.times { minter.mint.should =~ /^8rf\d\d$/ }
+    expect(minter.mint).to eq("8rf00")
+    10.times { expect(minter.mint).to match(/^8rf\d\d$/) }
+    expect(minter.mint).to eq("8rf11")
+    88.times { expect(minter.mint).to match(/^8rf\d\d$/) }
     expect { minter.mint }.to raise_exception
   end
 
   it "should mint sequential extended-digits" do
     minter = Noid::Minter.new(:template => '.se')
-    29.times.map { minter.mint }.join('').should == "0123456789bcdfghjkmnpqrstvwxz"
+    expect(29.times.map { minter.mint }.join('')).to eq("0123456789bcdfghjkmnpqrstvwxz")
   end
 
   it "should mint random 3-extended-digit numbers with constant prefix h9" do
     minter = Noid::Minter.new(:template => 'h9.reee')
 
-    (minter.template.max).times { minter.mint.should =~ /^h9\w\w\w$/ }
+    (minter.template.max).times { expect(minter.mint).to match(/^h9\w\w\w$/) }
     expect { minter.mint }.to raise_exception
   end
 
   it "should mint unlimited sequential numbers with at least 3 extended digits" do
     minter = Noid::Minter.new(:template => '.zeee')
-    (29*29*29).times { minter.mint.should =~ /^\w\w\w/ }
-    minter.mint.should =~ /^\w\w\w\w/
+    (29*29*29).times { expect(minter.mint).to match(/^\w\w\w/) }
+    expect(minter.mint).to match(/^\w\w\w\w/)
   end
 
   it "should mint random 7-char numbers, with extended digits at chars 2,4,and 5" do
     minter = Noid::Minter.new(:template => '.rdedeedd')
-    1000.times { minter.mint.should =~ /^\d\w\d\w\w\d\d$/ }
+    1000.times { expect(minter.mint).to match(/^\d\w\d\w\w\d\d$/) }
   end
 
   it "should mint unlimited mixed digits, adding new extended digits as needed" do
     minter = Noid::Minter.new(:template => '.zedededed')
-    minter.mint.should == "00000000"
+    expect(minter.mint).to eq("00000000")
   end
 
   it "should mint sequential 4-mixed-digit with constant prefix sdd" do
     minter = Noid::Minter.new(:template => 'sdd.sdede')
-    minter.mint.should == 'sdd0000'
-    1000.times { minter.mint.should =~ /^sdd\d\w\d\w$/}
-    minter.mint.should == "sdd034h"
+    expect(minter.mint).to eq('sdd0000')
+    1000.times { expect(minter.mint).to match(/^sdd\d\w\d\w$/)}
+    expect(minter.mint).to eq("sdd034h")
   end
 
   it "should mint random 3 mixed digits plus final (4th) computed check character" do
     minter = Noid::Minter.new(:template => '.rdedk')
-    1000.times { minter.mint.should =~ /^\d\w\d\w$/ }
+    1000.times { expect(minter.mint).to match(/^\d\w\d\w$/) }
   end
 
   it "should mint 5 sequential mixed digits plus final extended digit check char" do
     minter = Noid::Minter.new(:template => '.sdeeedk')
-    minter.mint.should == "000000"
-    minter.mint.should == "000015"
-    minter.mint.should == "00002b"
-    1000.times { minter.mint.should =~ /^\d\w\w\w\d\w$/ }
-    minter.mint.should == "003f3m"
+    expect(minter.mint).to eq("000000")
+    expect(minter.mint).to eq("000015")
+    expect(minter.mint).to eq("00002b")
+    1000.times { expect(minter.mint).to match(/^\d\w\w\w\d\w$/) }
+    expect(minter.mint).to eq("003f3m")
   end
 
   it "should mint sequential digits plus check char, with new digits added as needed" do
     minter = Noid::Minter.new(:template => ".zdeek")
-    minter.mint.should == "0000"
-    minter.mint.should == "0013"
-    (10*29*29-2).times { minter.mint.should =~ /^\d\w\w\w$/ }
-    minter.mint.should == "10001"
+    expect(minter.mint).to eq("0000")
+    expect(minter.mint).to eq("0013")
+    (10*29*29-2).times { expect(minter.mint).to match(/^\d\w\w\w$/) }
+    expect(minter.mint).to eq("10001")
   end
 
   it "should mint prefix plus random 3 mixed digits plus a check char" do
     minter = Noid::Minter.new(:template => "63q.redek")
-    minter.mint.should =~ /63q\w\d\w\w/
+    expect(minter.mint).to match(/63q\w\d\w\w/)
   end
 
   describe "validate" do
     it "should validate a prefixed identifier" do
       minter = Noid::Minter.new(:template => "foobar.redek")
       id = minter.mint
-      minter.valid?(id).should == true
+      expect(minter.valid?(id)).to eq(true)
     end
     it "should validate a prefixless identifier" do
       minter = Noid::Minter.new(:template => ".redek")
       id = minter.mint
-      minter.valid?(id).should == true
+      expect(minter.valid?(id)).to eq(true)
     end
     it "should validate with a new minter" do
       minter = Noid::Minter.new(:template => ".redek")
       id = minter.mint
       minter2 = Noid::Minter.new(:template => ".redek")
-      minter2.valid?(id).should == true
+      expect(minter2.valid?(id)).to eq(true)
     end
   end
 
@@ -118,11 +118,11 @@ describe Noid::Minter do
     it "given a specific seed, identifiers should be replicable" do
       minter = Noid::Minter.new(:template => "63q.redek")
       minter.seed(1)
-      minter.mint.should == "63q3706"
+      expect(minter.mint).to eq("63q3706")
 
       minter = Noid::Minter.new(:template => "63q.redek")
       minter.seed(1)
-      minter.mint.should == "63q3706"
+      expect(minter.mint).to eq("63q3706")
     end
 
     it "given a specific seed and sequence, identifiers should be replicable" do
@@ -137,7 +137,7 @@ describe Noid::Minter do
       dump2 = minter.dump
       expect(dump1).to eql(dump2)
       expect(mint1).to eql(mint2)
-      mint1.should == "63qb41v" # "63qh305" was the value from a slightly buggy impl
+      expect(mint1).to eq("63qb41v") # "63qh305" was the value from a slightly buggy impl
     end
   end
 
@@ -145,8 +145,8 @@ describe Noid::Minter do
     it "should dump the minter state" do
       minter = Noid::Minter.new(:template => ".sddd")
       d = minter.dump
-      d[:template].should == ".sddd"
-      d[:seq].should == 0
+      expect(d[:template]).to eq(".sddd")
+      expect(d[:seq]).to eq(0)
 
       minter.mint
       minter.mint
@@ -158,7 +158,7 @@ describe Noid::Minter do
       minter = Noid::Minter.new(:template => ".rddd")
       d = minter.dump
       d[:seq] == 0
-      d[:seed].should == minter.instance_variable_get('@seed')
+      expect(d[:seed]).to eq(minter.instance_variable_get('@seed'))
     end
 
     it "should allow a random identifier minter to be 'replayed' accurately" do
@@ -170,7 +170,7 @@ describe Noid::Minter do
 
       arr2 = 10.times.map { minter.mint }
 
-      arr.should == arr2
+      expect(arr).to eq(arr2)
 
     end
 
